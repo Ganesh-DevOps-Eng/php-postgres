690a32af3d8a0827112d82a98237e6b3acb51859
Ganesh-DevOps-Eng <ganeshjatms@gmail.com>
2024-08-12 23:01:04 +0530

    Update init_script.sh

--- init_script.sh
+++ init_script.sh
@@ -12,10 +12,11 @@ curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
 # Clone the repository and setup application
-cd /home/adminuser || exit
-rm -rf *
+mkdir -p /home/adminuser/project
+cd /home/adminuser/project
+git init
 git pull https://github.com/Ganesh-DevOps-Eng/php-postgres.git
-cd php-postgres/ || exit
+cd php-postgres/
 
 # Import database
 PGPASSWORD=H@Sh1CoR3! psql -h tom-psqlserver.postgres.database.azure.com -U psqladmin@tom-psqlserver -d postgres -f db.sql
@@ -28,14 +29,14 @@ echo "RewriteEngine On" | sudo tee -a /var/www/html/.htaccess
 echo "RewriteRule ^health$ health.php [L]" | sudo tee -a /var/www/html/.htaccess
 sudo chown www-data:www-data /var/www/html/*
 sudo chmod 644 /var/www/html/*
+
 # Update Apache configuration to allow overrides
 sudo bash -c 'cat <<EOT >> /etc/apache2/sites-available/000-default.conf
 <Directory /var/www/html>
     Options Indexes FollowSymLinks
     AllowOverride All
     Require all granted
-</Directory>
-EOT'
+</Directory>'
 
 # Install Composer dependencies
 yes | sudo composer require vlucas/phpdotenv
